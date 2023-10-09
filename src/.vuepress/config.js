7736c93608c7047cff79ad81c9a753988c99fe4e
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-10-09 18:02:47 -0300

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,35 +1,20 @@
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
   title: "A guide for donkeys",
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
   description: "Where technical teams document",
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
   head: [
     ['link', { rel: "icon", type: "image/png", sizes: "32x24", href: "/donkey_favicon_32_24.png" }],
     ['link', { rel: "icon", type: "image/png", sizes: "16x12", href: "/donkey_favicon_16_12.png" }],
     ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/donkey_favicon_shortcut.ico' }],
     ["meta", { name: "theme-color", content: "#3eaf7c" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-    [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-    ],
+    ['meta', { property: 'og:title', content: 'A guide for donkeys' }],
+		['meta', { property: 'og:description', content: 'Picture a guide... so simple even a donkey can grasp.' }],
+    ['meta', { property: 'og:image', content: '/donkey.png' }],
+    ['meta', { property: 'og:url', content: 'https://donkeyguide.vercel.app' }],
+    ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" } ],
   ],
 
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
     repo: "whoisdon/CommunityStructure",
     docsRepo: "whoisdon/CommunityStructure",
@@ -57,7 +42,8 @@ module.exports = {
           title: "Guide",
           collapsable: false,
           children: [
-            "installation-preparations"
+            "installation-preparations",
+            "starting-the-application"
           ],
         },
       ],
@@ -65,10 +51,6 @@ module.exports = {
   },
 
   theme: "yuu",
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     "@vuepress/plugin-back-to-top",
     "@vuepress/plugin-medium-zoom",
